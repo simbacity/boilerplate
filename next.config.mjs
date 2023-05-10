a49403cc952ed1df9b3aa327cc7925b6cdf4f980
danieltonel <danieltonel.xyz@gmail.com>
2023-05-10 22:48:40 +0200

    undo config changes

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ await import("./src/env.mjs");
 const config = {
   reactStrictMode: true,
   images: {
-    domains: ["images.clerk.dev", "boilerplate-six-iota.vercel.app"],
+    domains: ["images.clerk.dev"],
   },
 
   /**
