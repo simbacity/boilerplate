6e55c96fd8dcea9ee85b06bef40c26f2bbb2064d
danieltonel <danieltonel.xyz@gmail.com>
2023-05-10 22:55:27 +0200

    image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,13 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Image
-        src="/screenshot-app.png"
-        alt="screenshot of app"
-        width={2310}
-        height={1118}
-        className="rounded-t-3xl border border-b-0 shadow-2xl 2xl:rounded-3xl 2xl:border-b"
-      />
-    </>
+    <Image
+      src="/screenshot-app.png"
+      alt="screenshot of app"
+      width={2310}
+      height={1118}
+      className="rounded-t-3xl border border-b-0 shadow-2xl 2xl:rounded-3xl 2xl:border-b"
+    />
   );
 };
 
