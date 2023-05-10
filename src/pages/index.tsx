466ffa280664cb433949464910f6dabd835111f8
danieltonel <danieltonel.xyz@gmail.com>
2023-05-10 22:59:28 +0200

    remove classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ const Home: NextPage = () => {
       alt="screenshot of app"
       width={2310}
       height={1118}
-      className="rounded-t-3xl border border-b-0 shadow-2xl 2xl:rounded-3xl 2xl:border-b"
     />
   );
 };
